e984be522a404e59c7bc6b574d1db2ef6ab68ef3
Griffin Amaral <griffin.amaral98@gmail.com>
2023-08-04 02:14:43 -0400

    Added DOM manipulation to JavaScript code and updated HTML and CSS to reflect the changes

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
-const getComputerChoice = () => {
-  let rock = 1;
-  let paper = 2;
-  let selection = "";
-  let num = Math.floor(Math.random() * 3) + 1;
+let playerScore = 0;
+let computerScore = 0;
+let roundWinner = "";
 
-  if (num === rock) {
-    selection = "rock";
-  } else if (num === paper) {
-    selection = "paper";
-  } else {
-    selection = "scissors";
+const getComputerChoice = () => {
+  let selection = "";
+  let randNum = Math.floor(Math.random() * 3) + 1;
+
+  switch (randNum) {
+    case 1:
+      selection = "rock";
+      break;
+    case 2:
+      selection = "paper";
+      break;
+    case 3:
+      selection = "scissors";
   }
 
   return selection;
@@ -17,44 +22,84 @@ const getComputerChoice = () => {
 
 const playRound = (playerSelection, computerSelection) => {
   if (playerSelection === computerSelection) {
-    return "It's a tie!";
+    roundWinner = "Tie";
   } else if (
     (playerSelection === "rock" && computerSelection === "scissors") ||
     (playerSelection === "paper" && computerSelection === "rock") ||
     (playerSelection === "scissors" && computerSelection === "paper")
   ) {
-    return `You Win! ${playerSelection} beats ${computerSelection}`;
+    playerScore++;
+    roundWinner = "Player";
+    score.innerHTML = `Player: ${playerScore} - Computer: ${computerScore}`;
   } else {
-    return `You Lose! ${computerSelection} beats ${playerSelection}`;
+    computerScore++;
+    roundWinner = "Computer";
+    score.innerHTML = `Player: ${playerScore} - Computer: ${computerScore}`;
+  }
+  updateWinnerMessage(roundWinner, playerSelection, computerSelection);
+};
+
+const playerChoice = (playerSelection) => {
+  const computerSelection = getComputerChoice();
+  if (isGameOver()) {
+    modal();
+    gameWinner();
+    return;
+  }
+
+  playRound(playerSelection, computerSelection);
+  updateScore();
+
+  if (isGameOver()) {
+    modal();
+    gameWinner();
   }
 };
 
-const game = () => {
-  let playerScore = 0;
-  let computerScore = 0;
-
-  for (let i = 0; i < 5; i++) {
-    const playerSelection = prompt(
-      "Enter your selection of Rock, Paper, or Scissors: "
-    ).toLowerCase();
-    const computerSelection = getComputerChoice();
-    const result = playRound(playerSelection, computerSelection);
-
-    console.log(result);
-    if (result.startsWith("You Win!")) {
-      playerScore++;
-    } else if (result.startsWith("You Lose!")) {
-      computerScore++;
-    }
-  }
-
-  if (playerScore > computerScore) {
-    console.log(`You win the game!`);
-  } else if (computerScore > playerScore) {
-    console.log(`You lose the game!`);
-  } else {
-    console.log(`The game ended in a draw!`);
+updateScore = () => {
+  if (roundWinner === "Tie") {
+    document.querySelector(".winner-or-loser").innerHTML = "It's a tie!";
+  } else if (roundWinner === "Player") {
+    document.querySelector(".winner-or-loser").innerHTML = "You won!";
+  } else if (roundWinner === "Computer") {
+    document.querySelector(".winner-or-loser").innerHTML = "You lost!";
   }
 };
 
-game();
+const updateWinnerMessage = (winner, playerSelection, computerSelection) => {
+  if (winner === "Player") {
+    roundWinnerMessage.innerHTML = `${playerSelection} beats ${computerSelection}`;
+  } else if (winner === "Computer") {
+    roundWinnerMessage.innerHTML = `${computerSelection} beats ${playerSelection}`;
+  } else {
+    roundWinnerMessage.innerHTML = `${playerSelection} ties ${computerSelection}`;
+  }
+};
+
+const gameWinner = () => {
+  return playerScore > computerScore
+    ? (winner.innerHTML = "You Win!")
+    : (winner.innerHTML = "You Lose!");
+};
+
+const modal = () => {
+  playAgainModal.style.display = "block";
+};
+
+const isGameOver = () => playerScore == 5 || computerScore == 5;
+
+const rockBtn = document.querySelector(".rock");
+const paperBtn = document.querySelector(".paper");
+const scissorsBtn = document.querySelector(".scissors");
+const winner = document.querySelector(".winner");
+const score = document.querySelector(".score");
+const reset = document.querySelector(".reset");
+const playAgainModal = document.querySelector(".play-again");
+const roundWinnerMessage = document.querySelector(".round-winner");
+const scoreInfo = document.querySelector(".winner-or-loser");
+const scoreMessage = document.querySelector(".score");
+
+score.innerHTML = `Player: ${playerScore} - Computer: ${computerScore}`;
+rockBtn.addEventListener("click", () => playerChoice("rock"));
+paperBtn.addEventListener("click", () => playerChoice("paper"));
+scissorsBtn.addEventListener("click", () => playerChoice("scissors"));
